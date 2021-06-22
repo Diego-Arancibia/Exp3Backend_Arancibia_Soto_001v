fc86c5456d65bc36b145138ca2aa4e106e57bf4a
Leandro Patricio Soto Herrera <darancibia@duoc.cl>
2021-06-22 18:46:19 -0400

    configuracion de bd oracle

--- proyecto1/proyecto1/settings.py
+++ proyecto1/proyecto1/settings.py
@@ -80,7 +80,16 @@ WSGI_APPLICATION = 'proyecto1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   
+   'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xepdb1',
+        'USER': 'c##prueba003',
+        'PASSWORD': 'hola123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
     }
 }
 
